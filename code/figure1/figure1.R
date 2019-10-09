03dd64d4f68119937caa1cbd953590c634c76dce
Lin, Katie <klin9@bwh.harvard.edu>
2019-10-09 14:45:17 -0400

    replacing figure1 script

--- code/figure1/figure1.R
+++ code/figure1/figure1.R
@@ -1,95 +1,106 @@
 ################################################################################
 # prep
 ################################################################################
+rm(list=ls()) 
 # load packages
 library(plot0) 
 # set output path
 res_dir <- paste0(getwd(), '/results/')
 
 ################################################################################
-# figure 1b 
+# figure 1a 
 ################################################################################
-setup <- function(default_in_percentile = c(95, 97)) {
+# load -------------------------------------------------------------------------
+filepath <- paste0(getwd(), '/data')
+filename <- 'data_new.csv'
+dt <- MyFread(filename, filepath)
 
-    # load 
-    filepath <- paste0(getwd(), '/data')
-    filename <- 'data_new.csv'
-    cohort <- MyFread(filename, filepath)
+# compute ----------------------------------------------------------------------
+df <- MyComputePlotDF(dt, 
+                col.to.y = 'gagne_sum_t',
+                col.to.cut = 'risk_score_t',
+                col.to.groupby = 'race',
+                nquantiles = 10,
+                ci.level = 0.95)
+# labels -----------------------------------------------------------------------
+titlename <- ''
+subtitlename <- ''
+groupbycolorname <- 'Race'
+xname <- 'Percentile of Algorithm Risk Score'
+yname <- 'Number of Chronic conditions'
+color_scheme <- c("#000000","#999999") 
+linetype_scheme <- c('twodash', 'solid') 
 
-    cohort <- cohort[, c('race', 'risk_score_t', 'gagne_sum_t')]
-    dt <- cohort
-    
-    dt[, risk_pctile := cut(risk_score_t, unique(quantile(risk_score_t, probs=0:100/100)), include.lowest=TRUE, labels=FALSE), ]
-    
-    # enrollment stats: black and white enrollment and their ratio 
-    enroll_stats <- matrix(nrow = length(default_in_percentile), ncol = 3)
-    rownames(enroll_stats) <- default_in_percentile 
-    colnames(enroll_stats) <- c('black_before', 'black_after', 'ratio')
+default_in_percentile <- 97
+additional_screen_percentile <- 55
 
-    return(list(dt = dt,
-                enroll_stats = enroll_stats)) 
-}
+# plot -----------------------------------------------------------------------
+ga <- ggplot(data = df, aes(color = race, 
+                            linetype = race, 
+                            group = race)) +
+  theme_bw() +
+  labs(title = titlename, 
+       subtitle = subtitlename,
+       color = groupbycolorname,
+       x = xname,
+       y = yname) +
+   scale_x_continuous(breaks = seq(0, 100, 10)) +
+   scale_y_continuous(breaks = seq(0, 8, 2)) +
+   scale_color_manual(values = color_scheme, name = groupbycolorname) +
+   scale_linetype_manual(values = linetype_scheme, name = groupbycolorname) +
+   geom_point(aes(x = percentile, y = col_to_mean_by_percentile_by_race), alpha = 0.4, shape = 4) +
+   geom_point(aes(x = quantile - 5, y = col_to_mean_by_quantile_by_race), size = 2) +
+   geom_smooth(aes(x = percentile, y = col_to_mean_by_percentile_by_race), method = "glm", formula = y~x, method.args=list(family = gaussian(link = 'log')), se = FALSE, span = 0.99) +
+   geom_pointrange(aes(x = quantile - 5, y = col_to_mean_by_quantile_by_race, 
+                       ymin = col_to_mean_by_quantile_by_race - 1.96 * ci_se,
+                       ymax = col_to_mean_by_quantile_by_race + 1.96 * ci_se)) + 
+   geom_vline(aes(xintercept=default_in_percentile), colour="black", linetype="dashed") +
+   geom_text(aes(x=default_in_percentile, label="Defaulted into program", y = 6), colour="black", hjust = 1.2, size = 2) +
+   geom_vline(aes(xintercept=additional_screen_percentile), colour="dark gray", linetype="dashed") +
+   geom_text(aes(x=additional_screen_percentile, label="Referred for screen", y = 6), colour="dark gray", hjust = 1.2, size = 2) 
 
-exercise <- function(default_in_percentile){
+################################################################################
+# figure 1b
+################################################################################
+source(paste0(getwd(), '/code/figure1/figure1b.R'))
 
-    dt <- setup(default_in_percentile)$dt
-    enroll_stats <- setup(default_in_percentile)$enroll_stats
+# load -------------------------------------------------------------------------
+filepath <- paste0(getwd(), '/results')
+filename <- 'figure1b.csv'
+dt <- MyFread(filename, filepath)
 
-    for(j in seq_along(default_in_percentile)){
-        # enrolled 
-        prior_enrolled <- dt[risk_pctile >= default_in_percentile[j]]
+dt_long <- gather(dt, before_or_after, frac, before:after, factor_key=TRUE)
+dt_long$before_or_after <- as.factor(dt_long$before_or_after)
 
-        prior_w <- prior_enrolled[race == 'white']
-        prior_b <- prior_enrolled[race == 'black']
-        # prep 
-        upperb <- dt[risk_pctile >= default_in_percentile[j] & race == 'black']
-        upperw <- dt[risk_pctile >= default_in_percentile[j] & race == 'white']
-        lowerb <- dt[risk_pctile < default_in_percentile[j] & race == 'black']
+# labels -----------------------------------------------------------------------
+titlename <- ''
+subtitlename <- ''
+groupbycolorname <- ''
+xname <- 'Percentile of Algorithm Risk Score'
+yname <- 'Fraction Black'
 
-        # rank
-        upperw <- upperw[order(gagne_sum_t), ]
-        lowerb <- lowerb[order(-risk_score_t, -gagne_sum_t), ]
+program_size <- 90
 
-        # tracking comparisons
-        sw <- 1
-        sb <- 1
-        switched_count <- 0
-        switched_w <- NULL
-        switched_b <- NULL
-        while( sw < nrow(upperw)  & sb < nrow(lowerb)){
-            if(upperw[sw, gagne_sum_t] < lowerb[sb, gagne_sum_t]){
-                # keep track of marginal switched
-                switched_w <- rbind(switched_w, upperw[sw,]) %>% as.data.table
-                switched_b <- rbind(switched_b, lowerb[sb,]) %>% as.data.table
-                # update enrolled blacks
-                upperb <- rbind(upperb, lowerb[sb]) %>% as.data.table
-                # update enrolled whites 
-                upperw <- upperw[-sw,]
-                upperw <- upperw[order(gagne_sum_t),]
-                sw = sw + 1
-                sb = sb + 1
-                switched_count = switched_count + 1
-            }else{
-                sb = sb + 1
-                sw = sw
-                switched_count = switched_count
-            }
-        }
-        # calculate means 
-        sampw <- prior_w
-        sampb <- prior_b
+dt_long <- as.data.table(dt_long)
+dt_long <- dt_long[percentile >= 55]
+# plot ------------------------------------------------------------------------
 
-        black_before <- nrow(prior_b)/(nrow(prior_w) + nrow(prior_b))
-        black_after <- (nrow(prior_b) + switched_count)/(nrow(prior_w) + nrow(prior_b))
+gb <- ggplot(data = dt_long, aes(x = percentile, y = frac, color = before_or_after, group = before_or_after)) + theme_bw() + labs(title = titlename, subtitle = subtitlename,
+       color = groupbycolorname,
+       x = xname,
+       y = yname) +
+   scale_x_continuous(breaks = c(seq(55, 95, 5), 99)) +
+   scale_y_continuous(breaks = seq(0, 1, by = 0.05)) +
+   scale_color_manual(values = rev(color_scheme), labels = c("original", "simulated")) +
+   geom_point(shape = 4) +
+   geom_smooth(span = 0.99, se = TRUE, level = 0.95) + 
+   geom_vline(aes(xintercept=default_in_percentile), colour="black", linetype="dashed") +
+   geom_text(aes(x=default_in_percentile, label="Defaulted into program", y = 0.55), colour="black", hjust = 1.2, size = 2) 
 
-        ratio <- black_after / black_before
-        enroll_stats[j,] <- c(black_before, black_after, ratio)
-    }
-    return(enroll_stats = enroll_stats)
-}
-
-df <- exercise(default_in_percentile = seq(55, 99, 1)) %>% as.data.table
-setnames(df, c('black_before', 'black_after'), c('before', 'after'))
-df[, percentile:= seq(55,99,1)]
-
-write.csv(df, paste0(getwd(), '/results/figure1b.csv'), row.names = F)
+################################################################################
+# export 
+################################################################################
+# png 
+ggsave(paste0(res_dir, 'figure1.png'), grid.arrange(ga, gb, ncol = 2, respect=TRUE), width = 14, height = 7)
+# eps 
+ggsave(paste0(res_dir, 'figure1.eps'), device="eps", grid.arrange(ga, gb, ncol = 2, respect=TRUE), width = 14, height = 7)
